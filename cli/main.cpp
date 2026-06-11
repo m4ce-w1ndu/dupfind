c0ce1264518fdd33d37da6be34badc69170d8d89
m4ce-w1ndu <16584672+m4ce-w1ndu@users.noreply.github.com>
2026-06-11 14:38:34 +0200

    repo: reinitialized file structure

--- cli/main.cpp
+++
@@ -1,8 +0,0 @@
-#include "dedup/core/types.hpp"
-#include "dedup/core/detector.hpp"
-#include <iostream>
-
-int main(int argc, char* argv[]) {
-    std::cout << "dupfind CLI stub" << std::endl;
-    return 0;
-}
--- src/actions/reporter.cpp
+++
@@ -1,9 +0,0 @@
-#include "dedup/actions/reporter.hpp"
-
-namespace dedup {
-namespace actions {
-
-void Reporter::generateReport(const std::vector<DuplicateGroup>& groups, const std::string& outputPath) {}
-
-} // namespace actions
-} // namespace dedup
